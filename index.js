5d992186d8d72bdd8f6b39bb80a2842271feddc6
Pongsak Phongtui <boomspass@hotmail.com>
2021-10-11 19:34:45 +0700

    fix error heroku R10

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const Discord = require('discord.js');
 require('dotenv').config();
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
