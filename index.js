a35a983947e34d274b5994f4f49989b3acfbe084
Pongsak Phongtui <boomspass@hotmail.com>
2022-08-09 12:05:29 +0700

    Update Kardoi ytdl-core

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 //For Heroku Error R10 fix////////////////////////////////////////////////////////
-// var express = require('express');
-// var app     = express();
+var express = require('express');
+var app     = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 ///////////////////////////////////////////////////////////////////////////////////
 
 client.commands = new Discord.Collection();
@@ -29,4 +29,4 @@ client.on('ready', () => {
   client.user.setActivity('$help, มึงคิดว่ากูฉลาดมากหรือไง');
 });
 
-client.login(process.env.DISCORD_TOKEN_TEST);
+client.login(process.env.DISCORD_TOKEN);
