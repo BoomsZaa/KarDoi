cc22035c9cebef1465ce4fdb114672b0b57a808a
Pongsak Phongtui <boomspass@hotmail.com>
2023-06-04 00:30:58 +0700

    Update 04-06-2023

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 //For Heroku Error R10 fix////////////////////////////////////////////////////////
-var express = require('express');
-var app     = express();
+// var express = require('express');
+// var app     = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+// //For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// });
 ///////////////////////////////////////////////////////////////////////////////////
 
 client.commands = new Discord.Collection();
@@ -26,7 +26,7 @@ client.events = new Discord.Collection();
 
 client.on('ready', () => {
   // console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('$help, มึงคิดว่ากูฉลาดมากหรือไง');
+  client.user.setActivity('!help, มึงคิดว่ากูฉลาดมากหรือไง');
 });
 
 client.login(process.env.DISCORD_TOKEN);
